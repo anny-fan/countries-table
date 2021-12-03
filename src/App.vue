cc21cef8f9761a702653a386e0b39c830bd67c41
Anny <annyfan.yachun@gmail.com>
2021-12-03 10:52:43 +0800

    add countrycatalogue component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <CountryCatalogue
+    v-if="tableData"
+    :data="tableData"
+    :config="config"
+  ></CountryCatalogue>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CountryCatalogue from "./components/CountryCatalogue.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    CountryCatalogue,
+  },
+  data: () => ({
+    tableData: undefined,
+    config: [
+      {
+        key: "flag",
+        title: "國旗",
+        type: "img",
+      },
+      {
+        key: "name",
+        title: "國家名稱",
+        type: "text",
+      },
+      {
+        key: "alpha2Code",
+        title: "2位國家代碼",
+        type: "text",
+      },
+      {
+        key: "alpha3Code",
+        title: "3位國家代碼",
+        type: "text",
+      },
+      {
+        key: "nativeName",
+        title: "母語名稱",
+        type: "text",
+      },
+      {
+        key: "altSpellings",
+        title: "替代國家名稱",
+        type: "list",
+      },
+      {
+        key: "callingCodes",
+        title: "國際電話區號",
+        type: "textArray",
+      },
+    ],
+  }),
+  mounted() {
+    this.axios.get("https://restcountries.com/v2/all").then((response) => {
+      console.log(response.data.length);
+      this.tableData = response.data;
+      console.log(response);
+    });
   },
 };
 </script>
---
+++ src/components/CountryCatalogue.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container-lg">
+    <table class="table">
+      <thead>
+        <th v-for="obj in config" :key="obj.key">{{ obj.title }}</th>
+      </thead>
+      <tbody>
+        <tr v-for="(row, index) in data" :key="index">
+          <td v-for="(obj, index) in config" :key="index">
+            <img
+              v-if="obj.type === 'img'"
+              :src="row[obj.key]"
+              alt=""
+              width="40"
+            />
+            <span v-if="obj.type === 'text'">{{ row[obj.key] }}</span>
+            <ol
+              v-if="obj.type === 'list'"
+              class="list-group list-group-numbered"
+            >
+              <li
+                class="list-group-item"
+                v-for="item in row[obj.key]"
+                :key="item"
+              >
+                {{ item }}
+              </li>
+            </ol>
+            <span v-if="obj.type === 'textArray'">{{ row[obj.key][0] }}</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: ["data", "config"],
+  data: () => ({
+    // countries: [],
+  }),
+};
+</script>
+
+<style scoped lang="scss">
+th {
+  white-space: nowrap;
+  padding: 5px 10px;
+}
+</style>
